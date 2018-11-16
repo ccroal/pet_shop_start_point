cfe30c1f7dffa430e217bce7c7d381b93d11f19f
Christopher Croal <ccroal@ntlworld.com>
2018-11-16 12:35:49 +0000

    defined pets_by_breed_

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,9 +11,23 @@ def add_or_remove_cash(pet_shop, amount)
 end
 
 def pets_sold(pet_shop)
-  return @pet_shop[:admin][:pets_sold]
+  return pet_shop[:admin][:pets_sold]
 end
 
 def increase_pets_sold(pet_shop, number_of_pets)
-  @pet_shop[:admin][:pets_sold] = @pet_shop[:admin][:pets_sold] += number_of_pets
+  pet_shop[:admin][:pets_sold] = pet_shop[:admin][:pets_sold] += number_of_pets
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count()
+end
+
+def pets_by_breed(pet_shop, breed)
+  pets = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      pets.push(breed)
+    end
+  end
+  return pets
 end
